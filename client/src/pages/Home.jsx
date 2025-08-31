844dbf3dfad4b812c8b5aced4755c6ebf509be2e
Ankit <ankityash03@gmail.com>
2025-09-01 04:19:43 +0530

    Project completed till frontend

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testomonial from '../components/Testomonial'
+import NewsLetter from '../components/NewsLetter'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedDestination />
+      <ExclusiveOffers />
+      <Testomonial />
+      <NewsLetter />
+    </>
+  )
+}
+
+export default Home
